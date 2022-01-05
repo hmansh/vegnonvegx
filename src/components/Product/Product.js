2348279a4a2405f00298ae9f662ccbba9ee054a8
hmansh <himanshucasillas@gmail.com>
2022-01-05 19:57:48 +0530

    updated code

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { PropTypes } from 'prop-types';
 import Fav from '../../assets/favorite.svg';
 import NotFav from '../../assets/favorite_border.svg';
 import '../../style/Product.css';
 
-const useStyle = (props) => ({
+const useStyle = (data) => ({
   productImageContainer: {
-    backgroundImage: `url(${props.imgUrl})`,
+    backgroundImage: `url(${data.imgUrl})`,
     backgroundSize: 'contain',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center'
   },
   wishlistLogo: {
-    backgroundImage: `url(${props.inWishlist ? NotFav : Fav})`,
+    backgroundImage: `url(${data.inWishlist ? NotFav : Fav})`,
     backgroundRepeat: 'no-repeat'
   }
 });
 
 export default function Product(props) {
   const dispatch = useDispatch();
-  const data = props;
+  const { data } = props;
   const style = useStyle(data);
 
   return (
@@ -43,3 +44,9 @@ export default function Product(props) {
     </div>
   );
 }
+
+Product.propTypes = PropTypes.shape({
+  company: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+}).isRequired;
